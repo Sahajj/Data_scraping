875ae1d32bf4862ec2f22a2da22fbdcfee5aea91
sahaj9 <sahajjain739739@gmail.com>
2023-05-29 15:45:31 +0530

    added test.py

--- test.py
+++ test.py
@@ -1,13 +1,36 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.select import Select
-Driver_path = 'D:\data scraping\chrome_deriver\chromedriver.exe'
-driver = webdriver.Chrome()
-driver.get("https://www.tutorialspoint.com/selenium/selenium_automation_practice.htm")
-# identify dropdown with Select class
-sel = Select(driver.find_element("x-path","//select[@name='continents']"))
-#select by select_by_visible_text() method
-sel.select_by_visible_text("Europe")
-time.sleep(0.8)
-#select by select_by_index() method
-sel.select_by_index(0)
-driver.close()
+from selenium.webdriver.common.action_chains import ActionChains
+import time 
+import pandas as pd
+
+service = Service(r"chrome_driver/chromedrvier.exe")
+driver = webdriver.Chrome(service = service)
+driver.get('https://www.indeed.com/career/salaries/technology?from=whatwhere&b=technology')
+
+time.sleep(7)
+Button_element = driver.find_element(By.XPATH,"//h2[normalize-space()='Software Engineer']").click()
+time.sleep(5)
+Position = Button_element.text
+
+time.sleep(2)
+element = driver.find_element(By.XPATH, "//div[@class='css-15psvrv eu4oa1w0']")
+
+Pay = element.text
+
+time.sleep(2)
+data = {
+    Position : Pay
+}
+
+df = ps.DataFrame(data)
+
+file_path = 'data.csv'
+
+df.to_csv(file_path, index=False
+)
+time.sleep(10)
+
